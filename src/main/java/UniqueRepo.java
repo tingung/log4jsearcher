ef21bb9ec4a175894790f7039e2023fdf866bf60
hutingung <tingung.hu@aurea.com>
2021-12-15 16:26:44 +0800

    Check for the list of the unique repositories based on the product.

---
+++ src/main/java/UniqueRepo.java
@@ -0,0 +1,36 @@
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import org.apache.commons.io.FileUtils;
+
+public class UniqueRepo {
+
+    static final String GITHUB_TRILOGY_URL = "https://github.com/trilogy-group/";
+
+    public static void main(String[] args) throws IOException {
+        // change your path here
+        File c4model = new File("/home/hutingung/workspace/enghub/eng.hub/Products DS/" + args[0] + "/c4model.yaml");
+        List<String> inputs = FileUtils.readLines(c4model, StandardCharsets.UTF_8);
+        Set<String> repos = new HashSet<>();
+        inputs.forEach(line -> {
+            if (line.contains(GITHUB_TRILOGY_URL)) {
+                try {
+                    String repoName = line.substring(line.indexOf(GITHUB_TRILOGY_URL) + GITHUB_TRILOGY_URL.length());
+                    if (repoName.contains("/")) {
+                        repoName = repoName.substring(0, repoName.indexOf("/"));
+                    }
+                    repos.add(GITHUB_TRILOGY_URL + repoName);
+                } catch (Exception e) {
+                    System.err.println(line);
+                    e.printStackTrace();
+                }
+            }
+        });
+        repos.forEach(repo -> {
+            System.out.println(repo);
+        });
+    }
+}
